767c7046e62f7eb955d66d106f8c5a3000e5848e
Ming Ng <69211773+redconOne@users.noreply.github.com>
2022-09-23 15:20:13 -0600

    fix: removed put and delete routing

--- routes/scores.js
+++ routes/scores.js
@@ -4,11 +4,6 @@ const scoresController = require('../controllers/scores');
 const { ensureAuth } = require('../middleware/auth');
 
 router.get('/', ensureAuth, scoresController.getScores);
-
 router.post('/setScores', scoresController.setScores);
-
-router.put('/updateScores', scoresController.updateScores);
-
-router.delete('/deleteScores', scoresController.deleteScores);
 
 module.exports = router;
