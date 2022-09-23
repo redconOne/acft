e60b30027623e83d730aafd4c0189ca6151aa5ec
Ming Ng <69211773+redconOne@users.noreply.github.com>
2022-09-23 15:18:06 -0600

    fix: removed update and delete functions

--- controllers/scores.js
+++ controllers/scores.js
@@ -28,44 +28,4 @@ module.exports = {
       console.error(err);
     }
   },
-
-  updateScores: async (req, res) => {
-    const MDL = req.body.MDL,
-      SPT = req.body.SPT,
-      HRP = req.body.HRP,
-      SDC = req.body.SDC,
-      PLK = req.body.PLK,
-      TMR = req.body.TMR;
-
-    try {
-      await Scores.findOneAndUpdate(
-        {
-          _id: req.body.itemIdFromJSFile,
-        },
-        {
-          MDL: MDL,
-          SPT: SPT,
-          HRP: HRP,
-          SDC: SDC,
-          PLK: PLK,
-          TMR: TMR,
-        }
-      ).then((result) => {
-        console.log(`Score changed`);
-        res.json('Score Changed');
-      });
-    } catch (err) {
-      console.error(err);
-    }
-  },
-
-  deleteScores: async (req, res) => {
-    try {
-      await Scores.findOneAndDelete({ _id: req.body.itemIdFromJSFile });
-      console.log('Deleted Score');
-      res.json('Deleted Score');
-    } catch (err) {
-      console.error(err);
-    }
-  },
 };
